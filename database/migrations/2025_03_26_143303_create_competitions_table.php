2e9e61a98fd9cda1003ddbef8cf377b9a8776e25
vilsonnn <vilsonnn@student.telkomuniversity.ac.id>
2025-03-26 21:35:35 +0700

    update migrasi database fitur A.1 (coba ke main)

---
+++ database/migrations/2025_03_26_143303_create_competitions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('competitions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('category', 100);
+            $table->string('prize', 255);
+            $table->date('deadline');
+            $table->string('registration_link', 255);
+            $table->foreignId('organizer_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('competitions');
+    }
+};

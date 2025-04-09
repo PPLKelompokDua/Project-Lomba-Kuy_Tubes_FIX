a65a6d81404a5369d8a6f38e29030539ead53923
vilsonnn <vilsonnn@student.telkomuniversity.ac.id>
2025-04-09 14:30:32 +0700

    reset

--- resources/views/vendor/laravel-exceptions-renderer/navigation.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- resources/views/vendor/laravel-exceptions-renderer/navigation.blade.php -->
-<nav>
-    <!-- Placeholder Navigation -->
-    <ul class="flex space-x-4">
-        <li><a href="{{ route('welcome') }}" class="text-indigo-600">Home</a></li>
-        <li><a href="{{ route('dashboard') }}" class="text-indigo-600">Dashboard</a></li>
-    </ul>
-</nav>

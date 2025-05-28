635319b01823f973c7bfa43883dda7d6d8873371
angelyca21 <angelycasamosir8@gmail.com>
2025-05-28 21:15:30 +0700

    SCRUM-62

--- resources/views/team-recommendation/index.blade.php
+++ resources/views/team-recommendation/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-
-
 @section('content')
 <div class="container">
     <div class="card">
@@ -9,20 +7,24 @@
             <h2>Rekomendasi Teman Tim</h2>
         </div>
         <div class="card-body">
-            @foreach($recommendations as $recommendation)
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $recommendation['user']->name }}</h5>
-                    <p class="card-text">
-                        <strong>Kecocokan:</strong> {{ $recommendation['match_score'] }}%<br>
-                        <strong>Keahlian:</strong> {{ $recommendation['expertise'] }}<br>
-                        <strong>Gaya Kerja:</strong> {{ $recommendation['work_style'] }}
-                    </p>
-                    <a href="{{ route('messages.conversation', $recommendation['user']->id) }}" 
-                       class="btn btn-primary">Kirim Pesan</a>
-                </div>
-            </div>
-            @endforeach
+            @if($recommendations->isEmpty())
+                <p>Tidak ada rekomendasi saat ini.</p>
+            @else
+                @foreach($recommendations as $recommendation)
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ optional($recommendation['user'])->name }}</h5>
+                            <p class="card-text">
+                                <strong>Kecocokan:</strong> {{ $recommendation['match_score'] }}%<br>
+                                <strong>Keahlian:</strong> {{ $recommendation['expertise'] }}<br>
+                                <strong>Gaya Kerja:</strong> {{ $recommendation['work_style'] }}
+                            </p>
+                            <a href="{{ route('messages.conversation', $recommendation['user']->id) }}" 
+                               class="btn btn-primary">Kirim Pesan</a>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 </div>
